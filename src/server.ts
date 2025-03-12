34a0769be442b3c4714c23322d5d640ddf00f4d2
Nishant Prabhu <nishant.prabhu@techdenali.com>
2025-03-12 19:10:21 +0530

    files changed

--- src/server.ts
+++ src/server.ts
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || 5000;
 
 // Connect Mongodb
 connetDataBase().then((res: any) => {
-    console.log('res', res)
+   
 
     // Create HTTP server
     server.listen(PORT, () => {
